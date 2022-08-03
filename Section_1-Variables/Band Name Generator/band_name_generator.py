48805f3827af350dcd0bdf694e0508229f49de0c
Michael Spencer <ms53150@gmail.com>
2022-08-02 21:44:29 -0400

    Create band name generator

---
+++ Section_1-Variables/Band Name Generator/band_name_generator.py
@@ -0,0 +1,8 @@
+# Create a band name based on your home city and pet's name
+
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in? ")
+pet_name = input("What's your pet's name? ")
+band_name = city + " " + pet_name
+print("Your band name could be " + band_name)
+
---
+++ Section_1-Variables/Python Variables/python_variables.py
@@ -0,0 +1,11 @@
+# assign input to variable 'name'
+name = input("What is your name?")
+print(name)
+
+# Naming variables
+user_name = "peter_parker"
+
+# Notes:
+# - assigning variables is like saving phone numbers for people in your contacts
+# - when naming, use '_' to separate variable names ex. user_name
+
