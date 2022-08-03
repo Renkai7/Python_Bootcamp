ec774c820fe3b1b62bf976573c3edd8aedd380c3
Michael Spencer <ms53150@gmail.com>
2022-08-03 13:11:26 -0400

    Create data_type_basics.py

---
+++ Section_2-Data-Types/Data Types Basics/data_type_basics.py
@@ -0,0 +1,17 @@
+# Data Types
+
+# String
+#   Subscripting - pulling character out of a string
+print("Hello"[0])
+#   Using [-1] you can pull the last character in a string
+print("Hello"[-1])
+
+# Integer - whole numbers(numbers without decimals)
+print(123 + 345)
+
+# Float - Numbers using decimal places
+print(3.14141)
+
+# Boolean - True or False values
+truth = True
+deceit = False
