a9755cb163611c8a5c0dce2d749bdeacabcb43e6
Michael Spencer <ms53150@gmail.com>
2022-08-03 13:32:08 -0400

    Create data_type_exercise.py

---
+++ Section_2-Data-Types/Data Type Exercise/data_type_exercise.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+print(type(two_digit_number))
+num1 = two_digit_number[0]
+num2 = two_digit_number[1]
+result = int(num1) + int(num2)
+
+print(result)
