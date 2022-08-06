108c5cdaebc116222648f7b752ecfbec2bfc0e69
Michael Spencer <ms53150@gmail.com>
2022-08-05 20:00:44 -0400

    Add logic to check for even or odd number

--- Section_3-Logical-Operators/Logical Operator Basics/logical_operators-basics.py
+++ Section_3-Logical-Operators/Logical Operator Basics/logical_operators-basics.py
@@ -1,7 +1,13 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
+# Conditional operator
+#   check if height is greater than 120
+#   '==' operator checks if values are equal; only using a single '=' causes error
 if height > 120:
     print("You can ride the roller coaster!")
 else:
     print("Sorry! You do not meet the height requirement.")
+
+#   '%' is a modulo operator - divides numbers and gives remainder of division
+print(7 % 2)
