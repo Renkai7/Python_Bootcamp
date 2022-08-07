88ea8057641a18f0cfde74c8fbb81e80ed28f3be
Michael Spencer <ms53150@gmail.com>
2022-08-06 21:14:00 -0400

    Update logical_operators-basics.py

--- Section_3-Logical-Operators/Logical Operator Basics/logical_operators-basics.py
+++ Section_3-Logical-Operators/Logical Operator Basics/logical_operators-basics.py
@@ -13,14 +13,39 @@ height = int(input("What is your height in cm? "))
 print(7 % 2)
 
 # Nested if / else statement
+# if height > 120:
+#     print("You can ride the roller coaster!")
+#     age = int(input("What is your age? "))
+#     if age < 12:
+#         print("Please pay $5.")
+#     elif age <= 18:
+#         print("Please pay $7.")
+#     else:
+#         print("Please pay $12.")
+# else:
+#     print("Sorry! You do not meet the height requirement.")
+
+# Multiple if statements
+bill = 0
+
 if height > 120:
     print("You can ride the roller coaster!")
     age = int(input("What is your age? "))
     if age < 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Child tickets are $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
+        print("Youth tickets are $7.")
     else:
-        print("Please pay $12.")
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+
+    print(f"The bill total is ${bill}.")
 else:
     print("Sorry! You do not meet the height requirement.")
+
