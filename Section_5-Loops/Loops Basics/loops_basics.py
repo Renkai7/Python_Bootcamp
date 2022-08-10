3458fb63c832606044bc3a1a8da8f95e8ed78647
Michael Spencer <ms53150@gmail.com>
2022-08-09 21:36:02 -0400

    Create loops_basics.py

---
+++ Section_5-Loops/Loops Basics/loops_basics.py
@@ -0,0 +1,7 @@
+fruits = ["Apple", "Peach", "Pear"]
+# For loop
+#   takes list of 'fruits' and assigns variable: fruit to each item in list
+#   fruit value for each pass: 1st pass- fruit = Apple, 2nd pass- fruit = Peach, etc.
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
