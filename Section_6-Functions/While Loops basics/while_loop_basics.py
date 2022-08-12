8c34d3da615e28e4c0af8004903523b6776a6f9f
Michael Spencer <ms53150@gmail.com>
2022-08-11 20:30:46 -0400

    Create while_loop_basics.py

---
+++ Section_6-Functions/While Loops basics/while_loop_basics.py
@@ -0,0 +1,8 @@
+# Creating while loop
+# while something_is_true:
+#     Do something repeatedly
+
+# Notes
+#   While loops vs. For loops
+#       for loops - to iterate over something
+#       while loops - to carry out something until a condition you set is met
