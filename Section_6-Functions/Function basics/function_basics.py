3d5e19d1628a08dccb948d8ee3d0c2536e2f07f2
Michael Spencer <ms53150@gmail.com>
2022-08-11 20:30:51 -0400

    Create function_basics.py

---
+++ Section_6-Functions/Function basics/function_basics.py
@@ -0,0 +1,15 @@
+# Defining Functions
+# def function():
+#   Do this
+#   Then do this
+#   Finally do this
+
+# Create a function
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+# Execute function
+my_function()
+
