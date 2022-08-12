2b4f24b96bb5a47bff7c8ebd15834d52aad3d913
Michael Spencer <ms53150@gmail.com>
2022-08-11 21:31:06 -0400

    Update hangman.py

--- Section_7-Hangman-Project/hangman.py
+++ Section_7-Hangman-Project/hangman.py
@@ -1,17 +1,28 @@
 import random
 
 word_list = ["aardvark", "baboon", "camel"]
-
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+display = []
+# Choose random word from list
 chosen_word = random.choice(word_list)
-print(chosen_word)
+word_length = len(chosen_word)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+print(f'Pssst, the solution is {chosen_word}.')
+
+# Generate blanks for chosen word
+for _ in range(word_length):
+    display.append("_")
+print(display)
+
+# Ask user for a letter
 guess = input("Guess a letter: \n").lower()
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for letter in chosen_word:
-    if guess == letter:
-        print("Letter is in word")
-    else:
-        print("Not in word")
+# Check if user chosen letter is in chosen word
+for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = guess
+        print(position)
+print(display)
+
+
+
