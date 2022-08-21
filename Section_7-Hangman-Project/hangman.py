373ab3d495da0204dddaded7b23ff004d3acc03d
Michael Spencer <ms53150@gmail.com>
2022-08-21 11:30:46 -0400

    Add logic for hangman game

--- Section_7-Hangman-Project/hangman.py
+++ Section_7-Hangman-Project/hangman.py
@@ -1,7 +1,64 @@
 import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 word_list = ["aardvark", "baboon", "camel"]
 display = []
+lives = 6
 # Choose random word from list
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
@@ -13,16 +70,46 @@ for _ in range(word_length):
     display.append("_")
 print(display)
 
-# Ask user for a letter
-guess = input("Guess a letter: \n").lower()
+# Loop until user guesses all letters
+end_of_game = False
 
-# Check if user chosen letter is in chosen word
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = guess
-        print(position)
-print(display)
+# Display initial hangman
+print(stages[lives])
+while not end_of_game:
+
+    # Ask user for a letter
+    guess = input("Guess a letter: \n").lower()
+    # guess_length = len(guess)
+    # for position in range(guess_length):
+    #     if guess[position] in chosen_word:
+
+    # Check if you've already guessed a letter correctly
+    if guess in display:
+        print(f"You've already guessed '{guess}'")
+
+    # Check if user chosen letter is in chosen word
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = guess
+
+    # Check if letter is wrong guess then subtract 1 life
+    if guess not in chosen_word:
+        lives -= 1
+        # update hangman image
+        print(stages[lives])
+        print(f"'{guess}' is not in the word.")
+
+    # Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    # Check if out of lives or if all letters guessed correctly to end game
+    if "_" not in display:
+        end_of_game = True
+        print("Congrats! You win!")
+    elif lives == 0:
+        end_of_game = True
+        print("Out of lives. You lose.")
 
 
 
