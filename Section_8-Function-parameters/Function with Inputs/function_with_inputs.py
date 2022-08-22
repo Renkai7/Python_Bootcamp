9339d8bb4aa75282428a4b9c834cad473fa0e4af
Michael Spencer <ms53150@gmail.com>
2022-08-22 11:40:47 -0400

    Create function_with_inputs.py

---
+++ Section_8-Function-parameters/Function with Inputs/function_with_inputs.py
@@ -0,0 +1,24 @@
+# Regular Function
+def greet():
+    print("Hello Angela!")
+    print("Welcome to the program Jack Bauer.")
+    print("Shall we begin?")
+
+
+# greet()
+
+
+# Function that allows for input
+def greet_with_name(name):
+    print(f"Hello {name}!")
+    print(f"Welcome to the program {name}.")
+
+
+# greet_with_name("Angela")
+
+
+
+# Notes
+# Parameter vs. Argument
+# Parameter: the name of the data inside the function parenthesis (ex. "name" in def greet_with_name(name))
+# Argument: the value of the data used in function (ex. "Angela" in greet_with_name("Angela"))
