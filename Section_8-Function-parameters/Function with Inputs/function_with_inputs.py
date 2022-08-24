a67a0c7cfac710166e91ec37ddc5b3860c9fa1ae
Michael Spencer <ms53150@gmail.com>
2022-08-23 21:46:51 -0400

    Update function_with_inputs.py

--- Section_8-Function-parameters/Function with Inputs/function_with_inputs.py
+++ Section_8-Function-parameters/Function with Inputs/function_with_inputs.py
@@ -16,6 +16,18 @@ def greet_with_name(name):
 
 greet_with_name("Angela")
 
+
+# Function with more than 1 input
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+
+greet_with("Jack Bauer", "Nowhere")
+
+# Keyword Arguments
+greet_with(name="Angela", location="London")
+
 # Notes
 # Parameter vs. Argument
 # Parameter: the name of the data inside the function parenthesis (ex. "name" in def greet_with_name(name))
