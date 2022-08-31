cbb91dd1d84d58c518af78c3e7ba719d34c58056
Michael Spencer <ms53150@gmail.com>
2022-08-31 19:16:38 -0400

    Update caesar_cipher.py

--- Section_8-Function-parameters/Caesar Cipher/caesar_cipher.py
+++ Section_8-Function-parameters/Caesar Cipher/caesar_cipher.py
@@ -1,31 +1,39 @@
+from art import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(logo)
+isCipherOn = True
 
 
-def encrypt(message, shift_amount):
+def caesar(message, shift_amount, code_type):
     cipher_text = ""
     for letter in message:
-        index = alphabet.index(letter)
-        # restarts position based on remainder from larger number
-        new_position = (index + shift_amount) % len(alphabet)
-        cipher_text += alphabet[new_position]
+        if letter in alphabet:
+            index = alphabet.index(letter)
+            if code_type == "encode":
+                # restarts position based on remainder from larger number
+                new_position = (index + shift_amount) % len(alphabet)
+            else:
+                # Goes in reverse order for list when numbers are negative
+                new_position = index - shift_amount % len(alphabet)
 
-    print(f"This encoded text is {cipher_text}")
+            cipher_text += alphabet[new_position]
+        else:
+            cipher_text += letter
 
-
-def decrypt(message, shift_amount):
-    cipher_text = ""
-    for letter in message:
-        index = alphabet.index(letter)
-        new_position = index - shift_amount % len(alphabet)
-        cipher_text += alphabet[new_position]
-
-    print(f"This encoded text is {cipher_text}")
+    print(f"This {code_type}d text is {cipher_text}\n")
 
 
-# encrypt(message=text, shift_amount=shift)
-decrypt(message=text, shift_amount=shift)
+while isCipherOn:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(message=text, shift_amount=shift, code_type=direction)
+
+    continue_cipher = input("Would you like to continue using the cipher? 'yes' or 'no'\n").lower()
+
+    if continue_cipher == 'no':
+        isCipherOn = False
+        print("Goodbye!")
