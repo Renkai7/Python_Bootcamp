bc8b66fe931c5a0a88c503ef01f5f73b88a85161
Michael Spencer <ms53150@gmail.com>
2022-09-01 19:31:46 -0400

    Create function_outputs_basics.py

---
+++ Section_10-Functions_with_Outputs/Function with Outputs basics/function_outputs_basics.py
@@ -0,0 +1,18 @@
+# Functions with Outputs
+
+def format_name(f_name, l_name):
+    # Can have multiple returns in function
+
+    # Return if conditions are met
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+
+    return f"{formatted_f_name} {formatted_l_name}"
+
+
+formatted_string = format_name("mIchaEl", "sPENcEr")
+# print(formatted_string)
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
