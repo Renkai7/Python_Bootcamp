ed3b89aee6c08c428272d4bf7ef49357e25dd742
Michael Spencer <ms53150@gmail.com>
2022-09-02 17:23:46 -0400

    Update function_outputs_basics.py

--- Section_10-Functions_with_Outputs/Function with Outputs basics/function_outputs_basics.py
+++ Section_10-Functions_with_Outputs/Function with Outputs basics/function_outputs_basics.py
@@ -1,6 +1,10 @@
 # Functions with Outputs
 
 def format_name(f_name, l_name):
+    """
+    Take a first and last name and format it to return the title case version of the name.
+     
+    """
     # Can have multiple returns in function
 
     # Return if conditions are met
