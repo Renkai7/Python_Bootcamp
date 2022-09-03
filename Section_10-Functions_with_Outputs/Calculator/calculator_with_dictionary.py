c01be188be3faa12faa6a571426252322ea82d2b
Michael Spencer <ms53150@gmail.com>
2022-09-02 22:28:24 -0400

    Create recursion for calculator to loop

--- Section_10-Functions_with_Outputs/Calculator/calculator_with_dictionary.py
+++ Section_10-Functions_with_Outputs/Calculator/calculator_with_dictionary.py
@@ -1,5 +1,6 @@
 # Calculator
 
+
 # Add
 def add(n1, n2):
     return n1 + n2
@@ -28,25 +29,46 @@ math_operators = {
     "/": divide
 }
 
-# User chooses first number to calculate with
-num1 = int(input("What's the first number?: "))
 
-# Show all math operator options
-for operators in math_operators:
-    print(operators)
+def calculator():
+    # User chooses first number to calculate with
+    num1 = float(input("What's the first number?: "))
 
-# User chooses operation
-operation = input("Pick an operation from the line above: ")
+    # Show all math operator options
+    for operators in math_operators:
+        print(operators)
+    isCalculating = True
 
-# User chooses second number to calculate with
-num2 = int(input("What's the second number?: "))
+    while isCalculating:
+        # User chooses operation
+        operation = input("Pick an operation from the line above: ")
 
-# Get math function in dictionary
-calculation_function = math_operators[operation]
+        # User chooses second number to calculate with (only number user will update)
+        num2 = float(input("What's the next number?: "))
 
-# Assign chosen function to variable - answer
-answer = calculation_function(num1, num2)
+        # Get math function in dictionary
+        calculation_function = math_operators[operation]
 
-print(f"{num1} {operation} {num2} = {answer}")
+        # Assign calculation function to variable - answer
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation} {num2} = {answer}")
+
+        # Store previous answer as first number in operation
+        num1 = answer
+
+        # Check if user wants to continue
+        continue_calc = input(
+            f"Type 'y' to continue calculating with {answer}, type 'n' to start a new calculation, or 'exit' to Exit: ").lower()
+
+        if continue_calc == 'n':
+            # Recursion - calculator() being called within itself
+            calculator()
+        elif continue_calc == 'exit':
+            isCalculating = False
 
 
+calculator()
+# Notes
+# Recursion - function that calls itself
+# Recursion resets a function and all values within it (A new game!)
