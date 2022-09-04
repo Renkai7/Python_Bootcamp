e7a204262c2debd5a13208e62b05b57ab9b22308
Michael Spencer <ms53150@gmail.com>
2022-09-04 18:18:24 -0400

    Update nesting_list_and_dictionary.py

--- Section_9-Dictionaries/Nesting Lists and Dictionary/nesting_list_and_dictionary.py
+++ Section_9-Dictionaries/Nesting Lists and Dictionary/nesting_list_and_dictionary.py
@@ -11,16 +11,16 @@ capitals = {
 # }
 
 # Nesting a Dictionary in a Dictionary
-# travel_log = {
-#     "France": {
-#         "cities_visited": ["Paris", "Lille", "Dijon"],
-#         "total_visits:": 12
-#     },
-#     "Germany": {
-#         "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
-#         "total_visits": 9
-#     }
-# }
+travel_log = {
+    "France": {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits:": 12
+    },
+    "Germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 9
+    }
+}
 
 # Nesting a Dictionary in a List
 travel_log = [
