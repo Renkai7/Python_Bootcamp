47712663b0d9a57811d3832f0bdaedd5167039bc
Michael Spencer <ms53150@gmail.com>
2022-09-13 23:24:41 -0400

    Update OOP_Basics.py

--- Section_16-OOP/OOP_Basics/OOP_Basics.py
+++ Section_16-OOP/OOP_Basics/OOP_Basics.py
@@ -2,11 +2,21 @@ import turtle
 from turtle import Turtle, Screen
 
 # Setting Object to variable
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+
+print(table)
+
