1c494b4f4a31a8a468ab6a86ece6dff5c18866d5
Michael Spencer <ms53150@gmail.com>
2022-09-28 21:35:46 -0400

    Add file directory examples

---
+++ Section_24-Files-Directories-Paths/Open_File-Basics/main.py
@@ -0,0 +1,24 @@
+# Using this lets you give variable name to file and close file for you without file.close()
+# Read from file
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+# file = open("my_file.txt")
+# file.close()
+
+# Write to file
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\nNew text.")
+
+# Write to file that doesn't exist yet
+# with open("new_file.txt", mode="w") as file:
+#     file.write("New text.")
+
+# Notes
+# Use file.close() because it will use PC resources
+# To write to file you must use mode="w"
+# mode="w" (write) will overwrite previous text in file with new text
+# mode="a" (append) will add new text to file without overwriting
+# If you write using (mode="w") to a file that doesn't exist then it will be created automatically
+
--- Section_24-Files-Directories-Paths/Snake_Game_with_Highscore/scoreboard.py
+++ Section_24-Files-Directories-Paths/Snake_Game_with_Highscore/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -22,6 +23,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.update_high_score()
         self.score = 0
         self.update_scoreboard()
 
@@ -29,3 +31,7 @@ class Scoreboard(Turtle):
         self.score += 1
         self.clear()
         self.update_scoreboard()
+
+    def update_high_score(self):
+        with open("data.txt", mode="w") as file:
+            file.write(str(self.high_score))
