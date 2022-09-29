5a5efd4872574360515f7c5e6de94b91c6ffacbd
Michael Spencer <ms53150@gmail.com>
2022-09-29 12:10:53 -0400

    Complete mail merge project

--- Section_24-Files-Directories-Paths/Open_File-Basics/main.py
+++ Section_24-Files-Directories-Paths/Open_File-Basics/main.py
@@ -1,6 +1,6 @@
 # Using this lets you give variable name to file and close file for you without file.close()
 # Read from file
-with open("my_file.txt") as file:
+with open("Output/my_file.txt") as file:
     contents = file.read()
     print(contents)
 
@@ -14,6 +14,9 @@ with open("my_file.txt") as file:
 # Write to file that doesn't exist yet
 # with open("new_file.txt", mode="w") as file:
 #     file.write("New text.")
+
+# Relative and Absolute File Path example
+
 
 # Notes
 # Use file.close() because it will use PC resources
