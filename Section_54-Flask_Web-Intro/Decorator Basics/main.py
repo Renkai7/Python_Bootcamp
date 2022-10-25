10bc8c30ff2eff47c9dc0716156744416c377a62
Michael Spencer <ms53150@gmail.com>
2022-10-24 21:14:14 -0400

    Update main.py

--- Section_54-Flask_Web-Intro/Decorator Basics/main.py
+++ Section_54-Flask_Web-Intro/Decorator Basics/main.py
@@ -17,7 +17,9 @@ def delay_decorator(function):
 def say_hello():
     print("Hello")
 
-say_hello()
+
+# say_hello()
+
 
 # # With the @ syntactic sugar
 # @delay_decorator
@@ -32,3 +34,27 @@ say_hello()
 #
 # decorated_function = delay_decorator(say_greeting)
 # decorated_function()
+
+# Advanced Python Decorator Functions
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
+
+
+def is_authenticated_decorator(function):
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in:
+            function(args[0])
+
+    return wrapper
+
+
+@is_authenticated_decorator
+def create_blog_post(user):
+    print(f"This is {user.name}'s new blog post.")
+
+
+new_user = User("angela")
+new_user.is_logged_in = True
+create_blog_post(new_user)
