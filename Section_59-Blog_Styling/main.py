a7e497e22975e0b9e595b940a7bcd1355a8656a6
Michael Spencer <ms53150@gmail.com>
2022-11-02 20:33:18 -0400

    Create blog

---
+++ Section_59-Blog_Styling/main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+import requests
+from post import Post
+
+blog_url = "https://api.npoint.io/46f289398d104c3409ce"
+posts = requests.get(blog_url).json()
+post_objects = []
+
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"], post["author"], post["date"])
+    post_objects.append(post_obj)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/post/<int:index>')
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
