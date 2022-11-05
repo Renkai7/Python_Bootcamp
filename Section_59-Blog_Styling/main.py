d61aa6e8591ab63d3642901ff76ba7bfb651e48c
Michael Spencer <ms53150@gmail.com>
2022-11-04 23:21:52 -0400

    Add contact form post and get

--- Section_59-Blog_Styling/main.py
+++ Section_59-Blog_Styling/main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 from post import Post
+import smtplib
+import os
 
 blog_url = "https://api.npoint.io/46f289398d104c3409ce"
 posts = requests.get(blog_url).json()
@@ -13,6 +15,25 @@ for post in posts:
 app = Flask(__name__)
 
 
+def send_email(name, email, phone, message):
+    my_email = "pythonemailtest106@gmail.com"
+    password = os.environ.get("PASSWORD")
+    message = f"Subject: New message\n\n" \
+              f"Name: {name}\n" \
+              f"Email: {email}\n" \
+              f"Phone: {phone}\n" \
+              f"Message: {message}"
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="tonystark53150@gmail.com",
+            msg=message
+        )
+
+
 @app.route('/')
 def home():
     return render_template("index.html", all_posts=posts)
@@ -23,9 +44,23 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/contact')
+# @app.route('/contact')
+# def contact():
+#     return render_template("contact.html")
+
+
+@app.route('/contact', methods=['POST', 'GET'])
 def contact():
-    return render_template("contact.html")
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        message = request.form['message']
+        send_email(name, email, phone, message)
+
+        return render_template("contact.html", msg_sent=True)
+
+    return render_template("contact.html", msg_sent=False)
 
 
 @app.route('/post/<int:index>')
